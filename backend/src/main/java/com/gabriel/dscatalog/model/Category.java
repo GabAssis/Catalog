63f671d95542a1c273a51c499c1d36d02781682c
gabassis <gabriel.assis2309@gmail.com>
2024-05-20 12:06:58 -0300

    Category entity and controller created

---
+++ backend/src/main/java/com/gabriel/dscatalog/model/Category.java
@@ -0,0 +1,56 @@
+package com.gabriel.dscatalog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public Category() {}
+
+    public Category(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    //Getter and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //equals and hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
