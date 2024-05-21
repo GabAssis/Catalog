7d6f1207e3a05976388c592e2f5c41e0b8db070f
gabassis <gabriel.assis2309@gmail.com>
2024-05-21 14:12:57 -0300

    Change the List<CategoryDTO> for Page( Pagination and page filters)

--- backend/src/main/java/com/gabriel/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gabriel/dscatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import com.gabriel.dscatalog.services.exceptions.EntityNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,9 +24,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
-        List<Category> list = repository.findAll();
-        return list.stream().map(x -> new CategoryDTO(x)).toList();
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+        Page<Category> list = repository.findAll(pageRequest);
+        return list.map(x -> new CategoryDTO(x));
 
 
     }
